c9e942543917a21685390ad8eec9fc72377eb2c1
Chulwoo Jung <sdfghfwwz@gmail.com>
2025-06-21 15:51:11 +0200

    email Operator

---
+++ dags/dags_emial_operator.py
@@ -0,0 +1,17 @@
+from airflow.sdk import DAG
+import pendulum
+import datetime as dt
+from airflow.providers.smtp.operators.smtp import EmailOperator
+
+with DAG(
+    dag_id = 'dags_email_operator',
+    schedule = "0 8 1 * *",
+    start_date = pendulum.datetime(2025,1,1, tz="Europe/Berlin"),
+    catchup = False
+) as dag:
+    send_email_task = EmailOperator(
+        task_id = 'send_email_task',
+        to = 'sdfghfwwz@naver.com',
+        subject = 'Airflow Email Test',
+        html_content = 'Airflow Email Test'
+    )

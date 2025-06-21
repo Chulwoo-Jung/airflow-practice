9f6f508831547cf5ae8f5c78ba57a289a828260c
Chulwoo Jung <sdfghfwwz@gmail.com>
2025-06-21 23:15:07 +0200

    seoul property transaction dags commit

---
+++ dags/dags_seoul_property_transaction.py
@@ -0,0 +1,20 @@
+from operators.seoul_api_to_csv_operator import SeoulApiToCsvOperator
+import pendulum
+from airflow.sdk import DAG
+
+with DAG(
+    dag_id='dags_seoul_property_transaction',
+    schedule='0 7 * * *',
+    start_date=pendulum.datetime(2025,6,1, tz='Europe/Berlin'),
+    catchup=False
+) as dag:
+    seoul_property_transaction = SeoulApiToCsvOperator(
+        task_id='seoul_property_transaction',
+        dataset_nm='tbLnOpendataRtmsV',
+        path='./files/seoulPropertyTransaction/{{data_interval_end.in_timezone("Europe/Berlin") | ds_nodash }}',
+        file_name='seoulPropertyTransaction.csv'
+    )
+
+    seoul_property_transaction
+    
+  

94cbaba81eb997d531fc547291d5dcd2803b2a50
Chulwoo Jung <sdfghfwwz@gmail.com>
2025-06-22 16:08:43 +0200

    python Sensor - Seoul Covid19 Status

--- dags/dags_python_sensor.py
+++ dags/dags_python_sensor.py
@@ -40,7 +40,7 @@ with DAG(
 
     sensor_task = PythonSensor(
         task_id='sensor_task',
-        python_callable=check_api_update,
+        python_callable=check_api_update,   # This callable function must return True or False
         op_kwargs={'http_conn_id':'openapi.seoul.go.kr',
                    'endpoint':'{{var.value.apikey_openapi_seoul_go_kr}}/json/TbCorona19CountStatus',
                    'base_dt_col':'S_DT'},

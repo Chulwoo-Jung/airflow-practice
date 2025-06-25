5b89006ea1b11e870e46ad9760a02f45177c3beb
Chulwoo Jung <sdfghfwwz@gmail.com>
2025-06-26 00:24:00 +0200

    Weekly Stock Report with LLM to Email

--- dags/dags_stock_with_llm.py
+++ dags/dags_stock_with_llm.py
@@ -29,14 +29,12 @@ with DAG(
         import langchain_openai
         from langchain_core.output_parsers import StrOutputParser
         from langchain_core.prompts import PromptTemplate
-        from langchain.chains import LLMChain
         from airflow.models import Variable
         openai_api_key = Variable.get("openai_api_key")
         
         llm = langchain_openai.ChatOpenAI(
             api_key=openai_api_key,
-            model="gpt-4o-mini", 
-            temperature=0.2
+            model="o4-mini", 
         )
         parser = StrOutputParser()
         
@@ -49,17 +47,21 @@ with DAG(
         {stock_df_html}
         
         이 데이터를 기반으로 주식에 관한 메일을 보내는 글을 만들어주세요.
-        우선 {stock_df_html}를 보여주고, 그 다음에 인사말과 함께 이번주는 어떤 소식을 주목해야할지 먼저 알려주세요.
-        각 회사마다 꼭 봐야했던 주요한 이슈들을 같이 넣고, 
-        마지막에는 한주간 꼭 챙겨봐야할 뉴스 소식(중복되지 않게, 주요 경제소식)을 넣어주세요.
-        모든 글을 최종적으로 *꼭 html 형식*으로 만들어주세요.
+        우선 인사말과 함께 이번주는 어떤 소식을 주목해야할지 먼저 알려주세요. 그리고 모든 데이터를 보기좋게 가공해서 보여주세요.
+        각 회사마다 꼭 봐야했던 주요한 이슈들을 함께 넣고, 
+        마지막에는 한주간 꼭 챙겨봐야할 뉴스 소식(한국 뉴스로 중복되지 않게, 주요 경제소식)을 넣어주세요.
+        각각의 뉴스 소식에 3줄 정도의 설명도 함께 넣어주세요.
+        모든 글을 최종적으로 *꼭 html 형식*으로 만들어주세요. 꼭 *독자가 읽기 재밌도록 쓰세요.*
+        마치 GPT가 작성하지 않은 것 처럼 쓰세요.
         
         예시 형식:
         
-        <h2>이번주 주목해야 할 소식</h2>
-        <p>인사말과 함께 이번주는 어떤 소식을 주목해야할지 먼저 알려주세요.</p>
+        <h2>가장 주목할 뉴스 1개를 기반으로 친근한 제목을 만들어주세요. 이모티콘을 사용해도 됩니다.( ex = 끝나지 않는 관세 전쟁 ) </h2>
+        <p>인사말과 함께 이번주는 어떤 소식을 주목해야할지 먼저 알려주세요.(이번주는 유독 날씨가 추웠다, 미국 주식시장은 좋지 않았다, 중동 정세 불안으로 인해 주가가 낮아졌다, 등등)</p>
+    
         
         <h3>1. Apple (AAPL)</h3>
+        (여기에 {stock_df_html}에서 Name과 Ticker를 제외한 애플의 나머지 데이터를 보기좋게 가공해서 보여주세요)
         <p>애플의 주가 형성의 이유</p>
         <ul>
         <li>주가와 관련한 뉴스 1</li>
@@ -68,6 +70,7 @@ with DAG(
         </ul>
         
         <h3>2. Microsoft (MSFT)</h3>
+        (여기에 {stock_df_html}에서 Name과 Ticker를 제외한 마이크로소프트의 나머지 데이터를 보기좋게 가공해서 보여주세요)
         <p>마이크로소프트의 주가 형성의 이유</p>
         <ul>
         <li>주가와 관련한 뉴스 1</li>
@@ -76,6 +79,7 @@ with DAG(
         </ul>
         
         <h3>3. Google (GOOGL)</h3>
+        (여기에 {stock_df_html}에서 Name과 Ticker를 제외한 구글의 나머지 데이터를 보기좋게 가공해서 보여주세요)
         <p>구글의 주가 형성의 이유</p>
         <ul>
         <li>주가와 관련한 뉴스 1</li>
@@ -93,27 +97,36 @@ with DAG(
         <li>주요 뉴스 4</li>
         <li>주요 뉴스 5</li>
         </ul>
+        (여기서 추가로 생성하지 말고 끝내주세요)
         '''
         )
 
-        llm_chain = LLMChain(llm=llm, prompt=prompt, output_parser=parser)
-        response = llm_chain.invoke({"stock_df_html": stock_df_html})
+        chain = prompt | llm | parser
+        response = chain.invoke({"stock_df_html": stock_df_html})
         return response
     
     @task(task_id='send_email')
     def send_email(**kwargs):
         from airflow.providers.smtp.operators.smtp import EmailOperator        
+        from airflow.models import Variable
+        import pendulum
+        
         response = kwargs['ti'].xcom_pull(key='return_value', task_ids='get_output_from_llm')
 
-        email_list = '{{var.value.email_target}}'.split(',')
+        email_target = Variable.get("email_target")
+        email_list = email_target.split(',')
         email_list = [email.strip() for email in email_list]
 
-        EmailOperator(
+        formatted_date = pendulum.now('Europe/Berlin').strftime('%Y-%m-%d')
+        subject = f"🔥 M7 Titans: This Week's Market Movers | {formatted_date} | Weekly Report"
+        
+        email_op = EmailOperator(
             task_id='send_email',
             conn_id='conn_smtp_gmail',
             to=email_list,
-            subject='🔥 M7 Titans: This Week\'s Market Movers | {{ data_interval_end.in_timezone("Europe/Berlin") | ds }}',
+            subject=subject,
             html_content=response
         )
+        email_op.execute(context=kwargs)
 
     get_m7_stock_df() >> get_output_from_llm() >> send_email()
--- plugins/practical/get_stock_df.py
+++ plugins/practical/get_stock_df.py
@@ -1,6 +1,5 @@
 import yfinance as yf
 import pandas as pd
-from datetime import datetime, timedelta
 import warnings
 warnings.filterwarnings('ignore')
 
